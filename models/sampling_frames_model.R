145106cc4450cb44983f0c19659ef58c162dfaf7
Danielle Navarro <d.navarro@unsw.edu.au>
2021-06-28 18:17:08 +1000

    converts model to the actual hayes et al version

--- models/sampling_frames_model.R
+++ models/sampling_frames_model.R
@@ -1,149 +1,192 @@
 library(tidyverse)
-library(scico)
+library(mvtnorm)
+library(magrittr)
 
-sampling_frames_model <- function(theta = 1, smooth_weight = 1, mean_weight = 2) {
-  
-  # construct the hypothesis space: each row specifies the 
-  # value of the unknown function at each of the 6 test points
-  grid <- seq(.1, .9, .2)
-  hypotheses <- as_tibble(expand.grid(
-    test1 = grid,
-    test2 = grid,
-    test3 = grid,
-    test4 = grid,
-    test5 = grid,
-    test6 = grid
-  ))
-  
+# parameter dictionary:
+#
+# theta = the sampling assumption: 1 = strong, 0 = weak
+# tau = baseline in smoothness
+# rho = decay in smoothness constraint
+# sigma = inherent noise
+# mu = prior mean
+
+sampling_frames_model <- function(theta = 1, tau = 3, rho = 1, sigma = .5, mu = .05) {
+
+  # specify the locations of the test items
+  x_grid = 1:6
+
+  # specify possible values for the probability
+  y_grid <- seq(.1, .9, .2)
+
+  # logit function: phi is a logistic function of f, so
+  # f is a logit function of phi
+  logit <- function(phi) {
+    log(phi/(1-phi))
+  }
+
+  # define the psychological distance between values
+  distance <- function(xi, xj) {
+    abs(xi - xj)
+  }
+
+  # define the radial basis kernel function
+  rbf_kernel <- function(dist, tau, rho) {
+    tau^2 * exp(-rho * dist)
+  }
+
+  # construct base variance vector (to be added to the main
+  # diagonal of the covariance matrix)
+  add_noise <- function(Sigma, sigma) {
+    Sigma + diag(sigma^2, nrow(Sigma), ncol(Sigma))
+  }
+
+  # distance matrix between items is defined by the
+  # outer product...
+  distance_matrix <- outer(x_grid, x_grid, distance)
+
+  prior_mean <- function(mu) {
+    rep(mu, length(x_grid))
+  }
+
   # compute the priors (this is a placeholder)
-  model_prior <- function(hypotheses) {
-    
-    corr_distance <- with(hypotheses, {
-      abs(test2 - test1) + 
-        abs(test3 - test2) +
-        abs(test4 - test3) +
-        abs(test5 - test4) + 
-        abs(test6 - test5)
-    })
-    
-    mean_val <- 0
-    mean_distance <- with(hypotheses, {
-      abs(test1 - mean_val) + 
-        abs(test2 - mean_val) + 
-        abs(test3 - mean_val) + 
-        abs(test4 - mean_val) + 
-        abs(test5 - mean_val) + 
-        abs(test6 - mean_val) 
-    })
-    
-    distance <- corr_distance * smooth_weight + 
-      mean_distance * mean_weight  
-    
-    prior <- exp(-distance)
+  model_prior <- function(hypotheses_tbl) {
+
+    hypotheses_mat <- as.matrix(hypotheses_tbl)
+
+    covariance_matrix <- distance_matrix %>%
+      rbf_kernel(tau = tau, rho = rho) %>%
+      add_noise(sigma = sigma)
+
+    mean_vector <- prior_mean(mu = mu)
+
+    prior <- logit(hypotheses_mat) %>%
+      dmvnorm(mean = logit(mean_vector), sigma = covariance_matrix)
+
     prior <- prior / sum(prior)
-    
+
     return(prior)
   }
-  
-  # the samples observed by the participant occur only in 
+
+  # the samples observed by the participant occur only in
   # the locations corresponding to test1 and test2, and are
-  # always property positive. depending on condition the 
+  # always property positive. depending on condition the
   # participant may have seen 2, 8 or 20 items. for simplicity
-  # we assume that they are evenly split between test1 and 
+  # we assume that they are evenly split between test1 and
   # test 2. conditional on knowing the probabilities of at
   # each test point (and assuming the base rates for entities
   # at each location are uniform) the likelihood is proportional
   # to the function value at the location, where the normalisation
   # depends on the admissability rule specified by the frame
-  model_likelihood <- function(n, frame, hypotheses) {
-    
-    # theta is inherited from scope!
-    
+  model_likelihood <- function(n, frame, hypotheses_tbl) {
+
     if(frame == "property") {
-      
+
       # property sampling: must be positive property, can be
       # any location on the stimulus space
-      denominator <- with(hypotheses, {
+      denominator <- with(hypotheses_tbl, {
         test1 + test2 + test3 + test4 + test5 + test6
       })
-      
-    } 
-    
+
+    }
+
     if(frame == "category") {
-      
+
       # category sampling: can be positive or negative, but can
       # only be locations 1 or 2
       denominator <- 2
-      
+
     }
-    
+
     # compute likelihood for the two relevant elements of the sample space
     # (conditional on using the relevant sampling frame)
-    prob1 <- hypotheses$test1 / denominator
-    prob2 <- hypotheses$test2 / denominator
-    
+    prob1 <- hypotheses_tbl$test1 / denominator
+    prob2 <- hypotheses_tbl$test2 / denominator
+
     # weak sampling equivalent
-    weak1 <- hypotheses$test1 / 6
-    weak2 <- hypotheses$test2 / 6
-    
+    weak1 <- hypotheses_tbl$test1 / 6
+    weak2 <- hypotheses_tbl$test2 / 6
+
     # mixture
     prob1 <- theta * prob1 + (1 - theta) * weak1
     prob2 <- theta * prob2 + (1 - theta) * weak2
-    
-    # observations are conditionally iid so the probability of the sample 
+
+    # observations are conditionally iid so the probability of the sample
     # is the product  of the individual observations:
-    sample_likelihood <- prob1^(n/2) * prob2^(n/2) 
-    
+    sample_likelihood <- prob1^(n/2) * prob2^(n/2)
+
     return(sample_likelihood)
   }
-  
-  posterior_mean <- function(n, frame, hypotheses, prior) {
-    
-    likelihood <- model_likelihood(n, frame, hypotheses)
+
+  posterior_mean <- function(n, frame, hypotheses_tbl, prior) {
+
+    likelihood <- model_likelihood(n, frame, hypotheses_tbl)
     posterior <- likelihood * prior
     posterior <- posterior / sum(posterior)
-    
+
     return(tibble(
-      test1 = sum(posterior * hypotheses$test1),
-      test2 = sum(posterior * hypotheses$test2),
-      test3 = sum(posterior * hypotheses$test3),
-      test4 = sum(posterior * hypotheses$test4),
-      test5 = sum(posterior * hypotheses$test5),
-      test6 = sum(posterior * hypotheses$test6),
+      test1 = sum(posterior * hypotheses_tbl$test1),
+      test2 = sum(posterior * hypotheses_tbl$test2),
+      test3 = sum(posterior * hypotheses_tbl$test3),
+      test4 = sum(posterior * hypotheses_tbl$test4),
+      test5 = sum(posterior * hypotheses_tbl$test5),
+      test6 = sum(posterior * hypotheses_tbl$test6),
     ))
   }
-  
-  infer <- function(hypotheses) {
-    
-    prior <- model_prior(hypotheses)
+
+
+
+  # function to do the work -------------------------------------------------
+
+  infer <- function(hypotheses_tbl) {
+
+    prior <- model_prior(hypotheses_tbl)
     conditions <- as_tibble(expand.grid(
       n = c(2, 8, 20),
       frame = c("category", "property"),
       stringsAsFactors = FALSE
     ))
-    
+
     generalisations <- conditions %>%
       transpose() %>%
       map_dfr(~posterior_mean(.x$n, .x$frame, hypotheses, prior))
-    
+
     results <- bind_cols(conditions, generalisations)
-    results <- results %>% 
+    results <- results %>%
       pivot_longer(
-        cols = starts_with("test"), 
+        cols = starts_with("test"),
         names_to = "test_item",
         values_to = "response"
       ) %>%
       rename(
         "sampling_frame" = "frame",
         "sample_size" = "n"
-      ) %>% 
+      ) %>%
       mutate(
         test_item = as.numeric(str_remove_all(test_item, "test"))
       )
-    
+
     return(results)
   }
-  
+
+
+  # specify hypothesis space and run ----------------------------------------
+
+  # construct the hypothesis space: each row specifies the
+  # value of the unknown function at each of the 6 test points
+  hypotheses <- as_tibble(expand.grid(
+    test1 = y_grid,
+    test2 = y_grid,
+    test3 = y_grid,
+    test4 = y_grid,
+    test5 = y_grid,
+    test6 = y_grid
+  ))
+
   return(infer(hypotheses))
-  
+
 }
+
+model <- sampling_frames_model() %>% mutate(source = "model")
+dat <- bind_rows(model, human)
+print(plot_curves(dat))
+
